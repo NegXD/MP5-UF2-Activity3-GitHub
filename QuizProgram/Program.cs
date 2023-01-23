97770e2d6194e481c48f145ee57cb6e1a6db8870
Geng <a2208016@institutmontilivi.cat>
2023-01-23 17:45:27 +0100

    Agregar archivos de proyecto.

---
+++ QuizProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    #region Main
+    private static void Main(string[] args)
+    {
+        ConsoleKeyInfo tecla;
+        do
+        {
+            Console.Clear();
+            ShowOptions();
+            tecla = Console.ReadKey();
+            Console.Clear();
+            switch (tecla.Key)
+            {
+                case ConsoleKey.D0:
+                    MsgNextScreen("PRESS ANY KEY TO EXIT");
+                    break;
+                default:
+                    MsgNextScreen("ERROR. PRESS ANY KEY TO RETURN TO THE MENU");
+                    break;
+            }
+        }
+        while (tecla.Key != ConsoleKey.D0);
+    }
+
+    #endregion
+
+    #region Show Options and Message Next Screen
+    public static void ShowOptions()
+    {
+        Console.WriteLine("");
+        Console.WriteLine();
+        Console.WriteLine("0 - EXIT");
+    }
+
+    public static void MsgNextScreen(string msg)
+    {
+        Console.WriteLine(msg);
+        Console.ReadKey();
+    }
+
+    #endregion
+
+
+}
