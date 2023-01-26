a4c00db692adfbc949d6a8ca5ecbb60a4d12cf48
Geng <a2208016@institutmontilivi.cat>
2023-01-26 17:23:29 +0100

    Vesion 1 - C# Quest

--- QuizProgram/Program.cs
+++ QuizProgram/Program.cs
@@ -12,6 +12,9 @@
             Console.Clear();
             switch (tecla.Key)
             {
+                case ConsoleKey.D1:
+                    CShareQuiz();
+                    break;
                 case ConsoleKey.D0:
                     MsgNextScreen("PRESS ANY KEY TO EXIT");
                     break;
@@ -28,7 +31,7 @@
     #region Show Options and Message Next Screen
     public static void ShowOptions()
     {
-        Console.WriteLine("");
+        Console.WriteLine("1 - C# QUEST");
         Console.WriteLine();
         Console.WriteLine("0 - EXIT");
     }
@@ -41,5 +44,61 @@
 
     #endregion
 
+    public static void CShareQuiz()
+    {
+        ConsoleKeyInfo key;
+        int count = 0;
+        try
+        {
+            Console.WriteLine("C# is a [] paradigms");
+            Console.WriteLine("1. \tSimple");
+            Console.WriteLine("2. \tTriple");
+            Console.WriteLine("3. \tMultiple");
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D3) count++;
+            Console.Clear();
 
+            Console.WriteLine("The first year that appeared C# is ");
+            Console.WriteLine("1. \t1998");
+            Console.WriteLine("2. \t2000");
+            Console.WriteLine("3. \t2003");
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D2) count++;
+            Console.Clear();
+
+            Console.WriteLine("C# is from ");
+            Console.WriteLine("1. \tMinecrosoft");
+            Console.WriteLine("2. \tMincrosoft");
+            Console.WriteLine("3. \tMicrosoft");
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D3) count++;
+            Console.Clear();
+
+            Console.WriteLine("Who is the creator of C#? ");
+            Console.WriteLine("1. \tAnders Hejlsberg");
+            Console.WriteLine("2. \tNiklaus Wirth");
+            Console.WriteLine("3. \tMads Torgersen");
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D1) count++;
+            Console.Clear();
+
+            Console.WriteLine("The file extension of C# is ");
+            Console.WriteLine("1. \t.cs .csx .cshtml .razor");
+            Console.WriteLine("2. \t.cs .csx .cshtml .csprogram");
+            Console.WriteLine("3. \t.cs .csx .cshtml .crl");
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D1) count++;
+            Console.Clear();
+
+            Console.WriteLine($"You have {count} of 5 points");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            MsgNextScreen("PRESS A KEY TO GO TO THE MAIN MENU");
+        }
+    }
 }
