cb32f3d06d755f15bc96f13e90b23cbf62bfd632
oleksandr <a2208033@institutmontilivi.cat>
2023-01-30 19:06:47 +0100

    Version 3

--- QuizProgram/Program.cs
+++ QuizProgram/Program.cs
@@ -16,7 +16,10 @@
                     SistemaSolar();
                     break;
                 case ConsoleKey.D5:
-                    NameOfTheCapitals(();
+                    NameOfTheCapitals();
+                    break;
+                case ConsoleKey.D6:
+                    PermisosYComandosDeWindows();
                     break;
                 case ConsoleKey.D0:
                     MsgNextScreen("PRESS ANY KEY TO EXIT");
@@ -36,6 +39,7 @@
     {
         Console.WriteLine("4 - Questions about solar system");
         Console.WriteLine("5 - Name of the capitals");
+        Console.WriteLine("6 - Windows permissions and commands");
         Console.WriteLine("0 - EXIT");
     }
 
@@ -67,6 +71,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D2)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -89,6 +94,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D1)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -110,6 +116,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D3)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -131,6 +138,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D2)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -152,6 +160,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D3)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -174,7 +183,7 @@
 
     }
     
-    public string void NameOfTheCapitals()
+    public static void NameOfTheCapitals()
     {
         ConsoleKeyInfo tecla;
         MsgNextScreen("Hello, dear user, please answer the following questions on the topic indicated above and answer 5 question");
@@ -194,6 +203,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D1)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -216,6 +226,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D1)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -237,6 +248,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D1)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -258,6 +270,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D2)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -279,6 +292,7 @@
             Console.WriteLine();
             if (tecla.Key == ConsoleKey.D2)
             {
+                Console.WriteLine("That is the correct answer");
                 preguntasC++;
             }
             else
@@ -299,6 +313,136 @@
             MsgNextScreen("PRESS A KEY TO GO TO THE MAIN MENU");
         }
     }
+    
+    public static void PermisosYComandosDeWindows()
+    {
+        ConsoleKeyInfo tecla;
+        MsgNextScreen("Hello, dear user, please answer the following questions on the topic indicated above and answer 5 question");
+        int preguntasC = 0;
+        int preguntasIn = 0;
+        try
+        {
+            Console.WriteLine("Our Windows permissions and commands");
 
-   
+            Console.WriteLine("Question 1");
+
+            Console.WriteLine("What does this command do (cd) ? --> ");
+            Console.WriteLine("1 - It is used to change directory, using the formula *cd < DirectoryPath >* to go to the specific directory or folder that you indicate, or *cd..* (with a colon) to exit a folder and go to the beginning level or folder where he was staying.");
+            Console.WriteLine("2 - Close the Windows console window");
+            Console.WriteLine("3 - It shows all the commands that are available, putting in each one a brief description in English.");
+            tecla = Console.ReadKey();
+            Console.WriteLine();
+            if (tecla.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine("That is the correct answer");
+                preguntasC++;
+            }
+            else
+            {
+                preguntasIn++;
+                Console.WriteLine("The correct answer is 1 - It is used to change directory, using the formula *cd < DirectoryPath >* to go to the specific directory or folder that you indicate, or *cd..* (with a colon) to exit a folder and go to the beginning level or folder where he was staying");
+            } 
+
+            Console.WriteLine("Please click on any character to continue with the next question");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 2");
+
+            Console.WriteLine("What does this command do rename --> ");
+            Console.WriteLine("1 - Be very careful with this last basic command, because it is used to format the unit");
+            Console.WriteLine("2 - It allows you to change the name of the file that you consider appropriate, and even including its extension you can also change it.");
+            Console.WriteLine("3 - Copy one or more files to the address of your choice.");
+            tecla = Console.ReadKey();
+            Console.WriteLine();
+            if (tecla.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine("That is the correct answer");
+                preguntasC++;
+            }
+            else
+            {
+                preguntasIn++;
+                Console.WriteLine("The correct answer is 2 - It allows you to change the name of the file that you consider appropriate, and even including its extension you can also change it.");
+            } 
+            Console.WriteLine("Please click on any character to continue with the next question");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 3");
+
+            Console.WriteLine("What does this command do poweroff ? --> ");
+            Console.WriteLine("1 - Is not a Windows command. Is from Linux");
+            Console.WriteLine("2 - Open new window");
+            Console.WriteLine("3 - Close the Windows console window of a another element");
+            tecla = Console.ReadKey();
+            Console.WriteLine();
+            if (tecla.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine("That is the correct answer");
+                preguntasC++;
+            }
+            else
+            {
+                Console.WriteLine("The correct answer is 1 - Is not a Windows command. Is from Linux");
+                preguntasIn++;
+            }
+            Console.WriteLine("Please click on any character to continue with the next question");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 4");
+
+            Console.WriteLine("What does this command do dir ? --> ");
+            Console.WriteLine("1 - It shows you the directory tree of a specific folder that you tell it");
+            Console.WriteLine("2 - The command lists the contents of the directory or folder where you are, showing all the subfolders or files it contains. With this command you will be able to know if the file you are looking for is there or to which subfolder to navigate");
+            Console.WriteLine("3 - I don't nou");
+            tecla = Console.ReadKey();
+            Console.WriteLine();
+            if (tecla.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine("That is the correct answer");
+                preguntasC++;
+            }
+            else
+            {
+               preguntasIn++;
+               Console.WriteLine("The correct answer is 2 - The command lists the contents of the directory or folder where you are, showing all the subfolders or files it contains. With this command you will be able to know if the file you are looking for is there or to which subfolder to navigate");
+            }
+            Console.WriteLine("Please click on any character to continue with the next question");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Question 5");
+
+            Console.WriteLine("What does this command do tree? --> ");
+            Console.WriteLine("1 - It shows you the directory tree of a specific folder that you tell it");
+            Console.WriteLine("2 - It will return the tree of folders below the path where we are, but if we execute it as tree /f, it will also show us all the files that each of the directories contains in the form of a tree as well");
+            Console.WriteLine("3 - The command lists the contents of the directory or folder where you are, showing all the subfolders or files it contains. With this command you will be able to know if the file you are looking for is there or to which subfolder to navigate");
+            tecla = Console.ReadKey();
+            Console.WriteLine();
+            if (tecla.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine("That is the correct answer");
+                preguntasC++;
+            }
+            else
+            {
+                preguntasIn++;
+                Console.WriteLine("The correct answer is 2 - It will return the tree of folders below the path where we are, but if we execute it as tree /f, it will also show us all the files that each of the directories contains in the form of a tree as well");
+            }
+
+            Console.WriteLine($"You have corrected {preguntasC} aces failing {preguntasIn} of 5 questions");
+           
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            MsgNextScreen("PRESS A KEY TO GO TO THE MAIN MENU");
+        }
+    }
+    
 }
